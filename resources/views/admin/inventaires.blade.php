f05e5839956239dfd34f484cd015714273122c5f
aminelagh <amine.laghlabi@gmail.com>
2018-04-23 17:46:33 +0100

    inventaire done

--- resources/views/admin/inventaires.blade.php
+++ resources/views/admin/inventaires.blade.php
@@ -90,7 +90,7 @@
                     {{-- Total --}}
                     <div class="form-group has-feedback">
                       <label>Total</label>
-                      <input type="text" class="form-control" value=""  id="total"  name="total" disabled>
+                      <input type="text" class="form-control" value=""  id="total"  name="total" readonly>
                     </div>
                   </div>
                 </div>
@@ -113,13 +113,12 @@
       <div class="box">
         <header class="dark">
           <div class="icons"><i class="fa fa-check"></i></div>
-          <h5>Inventaire <span class="badge badge-info badge-pill" title="Nombre d'articles"> {{ $data->count() }}</span></h5>
+          <h5>Inventaire <span class="badge badge-info badge-pill" title="Nombre d'inventaires"> {{ $data->count() }}</span></h5>
           <div class="toolbar">
             <nav style="padding: 8px;">
               <a href="#" class="btn btn-info btn-xs" data-toggle="dropdown" title="Options"><i class="fa fa-bars"></i></a>
               <ul class="dropdown-menu">
-                <li><a href="#" onclick="exportArticlesFunction()">export</a></li>
-                <li><a data-toggle="modal" href="#modalAddArticles">Import</a></li>
+                <li><a href="#" onclick="exportInventairesFunction()">export</a></li>
               </ul>
               <div class="btn-group">
                 <a href="javascript:;" class="btn btn-default btn-xs collapse-box" title="Réduire"><i class="fa fa-minus"></i></a>
@@ -129,101 +128,97 @@
             </nav>
           </div>
         </header>
+
         <div id="collapse" class="body">
-          <!--div class="breadcrumb">
-          Afficher/Masquer:
-          <a class="toggle-vis" data-column="0">Code</a> -
-          <a class="toggle-vis" data-column="1">Famille</a> -
-          <a class="toggle-vis" data-column="2">Site</a> -
-          <a class="toggle-vis" data-column="3">Unité</a>
-        </div-->
-        <div class="breadcrumb">
-          <h4>Filtre</h4>
-          <form id="formFilterArticles" method="POST" action="{{ route('articles') }}">
-            @csrf
-            <div class="row">
-              <div class="col-lg-4">
-                {{-- Site
-                <div class="form-group has-feedback">
-                <label>Site</label>
-                <select  class="form-control" name="id_site" id="filter_id_site">
-                <option value="null"></option>
-                @foreach ($articles as $item)
-                <option value="{{ $item->id_article }}" {{ isset($selected_id_site) && $selected_id_site == $item->id_site ? 'selected' : ''  }}>{{ $item->libelle }} ({{ $item->libelle_societe }})</option>
+
+          <div class="breadcrumb">
+            <h4>Filtre</h4>
+            <form id="formFilterInventaires" method="POST" action="{{ route('inventaires') }}">
+              @csrf
+              <div class="row">
+                <div class="col-md-4">
+                  {{-- Article --}}
+                  <div class="form-group has-feedback">
+                    <label>Article</label>
+                    <select class="form-control selectpicker show-tick" data-live-search="true" name="id_article_site" id="filter_id_article_site">
+                      <option value="null">Tous les articles</option>
+                      @foreach ($articles as $item)
+                        <option value="{{ $item->id_article_site }}" {{ isset($selected_id_article_site) && $selected_id_article_site == $item->id_article_site ? 'selected' : ''  }}>{{ $item->code }} - {{ $item->designation }} ({{ $item->libelle_site }})</option>
+                      @endforeach
+                    </select>
+                  </div>
+                </div>
+                <div class="col-lg-4">
+                  {{-- Zone --}}
+                  <div class="form-group has-feedback">
+                    <label>Zone</label>
+                    <select  class="form-control" name="id_zone" id="filter_id_zone">
+                      <option value="null">Toutes les zones</option>
+                      @foreach ($zones as $item)
+                        <option value="{{ $item->id_zone }}" {{ isset($selected_id_zone) && $selected_id_zone == $item->id_zone ? 'selected' : ''  }}>{{ $item->libelle_zone }}</option>
+                      @endforeach
+                    </select>
+                  </div>
+                </div>
+                <div class="col-lg-2"></div>
+                <div class="col-lg-2"><br>
+                  <input type="submit" class="btn btn-primary" value="Filter" name="submitFiltre">
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div class="breadcrumb">
+            Afficher/Masquer:
+            <a class="toggle-vis" data-column="0">Article</a> -
+            <a class="toggle-vis" data-column="1">Zone</a> -
+            <a class="toggle-vis" data-column="2">Date</a> -
+            <a class="toggle-vis" data-column="3">Quantité</a> -
+            <a class="toggle-vis" data-column="4">Créé par</a>
+          </div>
+
+          <table id="inventairesTable" class="display table table-hover table-striped table-bordered" cellspacing="0" width="100%">
+            <thead><tr><th>Article</th><th>Zone</th><th>Date</th><th>Quantité</th><th>Créé par</th><th>Outils</th></tr></thead>
+            <tfoot><tr><th>Article</th><th>Zone</th><th>Date</th><th>Quantité</th><th>Créé par</th><th></th></tr></tfoot>
+            <tbody>
+              @foreach($data as $item)
+                <tr>
+                  <td>{{ $item->code }} - {{ $item->designation }}</td>
+                  <td>{{ $item->libelle_zone }}</td>
+                  <td>{{ $item->date }}</td>
+                  <td>{{ $item->nombre_palettes }}x{{ $item->nombre_pieces }}={{ $item->nombre_palettes*$item->nombre_pieces }} {{ $item->libelle_unite }}</td>
+                  <td>{{ $item->created_by_nom }} {{ $item->created_by_prenom }}</td>
+                  <td align="center">
+                    <i class="fa fa-edit" data-placement="bottom" data-original-title="Modifier" data-toggle="tooltip"
+                    onclick='updateInventaireFuntion({{ $item->id_inventaire }},{{ $item->id_article_site }},{{ $item->id_zone }},"{{ $item->date }}", {{ $item->nombre_palettes }}, {{ $item->nombre_pieces }} );' title="Modifier" ></i>
+                    {{--<i class="fa fa-edit" data-toggle="modal" data-target="#modalUpdateArticle" onclick='updateArticleFunction({{ $item->id_article }},{{ $item->id_categorie }},{{ $item->id_zone }},{{ $item->id_unite }},"{{ $item->code }}","{{ $item->designation }}" );' title="Modifier" ></i> --}}
+                    <i class="glyphicon glyphicon-trash" onclick="deleteInventaireFunction({{ $item->id_inventaire }},'{{ $item->designation }}','{{ $item->date }}');" data-placement="bottom" data-original-title="Supprimer" data-toggle="tooltip"></i>
+                  </td>
+                </tr>
               @endforeach
-            </select>
-          </div>--}}
+            </tbody>
+          </table>
         </div>
-        <div class="col-lg-4">
-          {{-- Famille
-          <div class="form-group has-feedback">
-          <label>Famille</label>
-          <select  class="form-control" name="id_famille" id="filter_id_famille">
-          <option value="null"></option>
-          @foreach ($zones as $item)
-          <option value="{{ $item->id_famille }}" {{ isset($selected_id_famille) && $selected_id_famille == $item->id_famille ? 'selected' : ''  }}>{{ $item->libelle }}</option>
-        @endforeach
-      </select>
+      </div>
+      {{-- *********************************** Articles ************************************* --}}
     </div>
-    --}}
   </div>
-  <div class="col-lg-2"></div>
-  <div class="col-lg-2"><br>
-    <input type="submit" class="btn btn-primary" value="Filter" name="submitFiltre">
-  </div>
-</div>
-</form>
-<div class="breadcrumb">
-  Afficher/Masquer:
-  <a class="toggle-vis" data-column="0">Article</a> -
-  <a class="toggle-vis" data-column="1">Zone</a> -
-  <a class="toggle-vis" data-column="2">Date</a> -
-  <a class="toggle-vis" data-column="3">Quantité</a> -
-  <a class="toggle-vis" data-column="4">Créé par</a>
-</div>
-</div>
-<table id="inventairesTable" class="display table table-hover table-striped table-bordered" cellspacing="0" width="100%">
-  <thead><tr><th>Article</th><th>Zone</th><th>Date</th><th>Quantité</th><th>Créé par</th><th>Outils</th></tr></thead>
-  <tfoot><tr><th>Article</th><th>Zone</th><th>Date</th><th>Quantité</th><th>Créé par</th><th></th></tr></tfoot>
-  <tbody>
-    @foreach($data as $item)
-      <tr>
-        <td>{{ $item->code }} {{ $item->designation }}</td>
-        <td>{{ $item->libelle_zone }}</td>
-        <td>{{ $item->date }}</td>
-        <td>{{ $item->nombre_palettes }}x{{ $item->nombre_pieces }}={{ $item->nombre_palettes*$item->nombre_pieces }} {{ $item->libelle_unite }}</td>
-        <td>{{ $item->created_by_nom }} {{ $item->created_by_prenom }}</td>
-        <td align="center">
-          <i class="fa fa-edit" data-placement="bottom" data-original-title="Modifier" data-toggle="tooltip"
-          onclick='updateArticleFunction({{ $item->id_article }},{{ $item->id_article }},"{{ $item->id_article }}" );' title="Modifier" ></i>
-          {{--<i class="fa fa-edit" data-toggle="modal" data-target="#modalUpdateArticle" onclick='updateArticleFunction({{ $item->id_article }},{{ $item->id_categorie }},{{ $item->id_zone }},{{ $item->id_unite }},"{{ $item->code }}","{{ $item->designation }}" );' title="Modifier" ></i> --}}
-          <i class="glyphicon glyphicon-trash" onclick="deleteArticleFunction({{ $item->id_article }},'{{ $item->id_article }}');" data-placement="bottom" data-original-title="Supprimer" data-toggle="tooltip"></i>
-        </td>
-      </tr>
+
+  <hr>
+  <div class="row">
+    <div class="col-md-12">
+      {{-- *********************************** Zones ************************************* --}
+
+      <table class="articlesHere endless-pagination" data-next-page="{{ $articles->nextPageUrl() }}" border="1" cellspacing="0">
+      <thead>  <tr><th>id</th><th>ip</th><th>type</th><th>created_at</th></tr></thead>
+      @foreach($articles as $post)
+      <tr><td>{{ $post->id }}</td><td>{{ $post->ip }}</td><td>{{ $post->type }}</td><td>{{ $post->created_at }}</td></tr>
     @endforeach
-  </tbody>
-</table>
-</div>
-</div>
-{{-- *********************************** Articles ************************************* --}}
-</div>
-</div>
+    {{-- {!! $posts->render() !!} --}
+    <tfoot><tr><th colspan="4">    <button class="btn btn-default" onclick="loadMore()">LoadMore</button></th></tr></tfoot>
+  </table>
 
-<hr>
-<div class="row">
-  <div class="col-md-12">
-    {{-- *********************************** Zones ************************************* --}
-
-    <table class="articlesHere endless-pagination" data-next-page="{{ $articles->nextPageUrl() }}" border="1" cellspacing="0">
-    <thead>  <tr><th>id</th><th>ip</th><th>type</th><th>created_at</th></tr></thead>
-    @foreach($articles as $post)
-    <tr><td>{{ $post->id }}</td><td>{{ $post->ip }}</td><td>{{ $post->type }}</td><td>{{ $post->created_at }}</td></tr>
-  @endforeach
-  {{-- {!! $posts->render() !!} --}
-  <tfoot><tr><th colspan="4">    <button class="btn btn-default" onclick="loadMore()">LoadMore</button></th></tr></tfoot>
-</table>
-
-{{-- *********************************** articles ************************************* --}}
+  {{-- *********************************** articles ************************************* --}}
 </div>
 </div>
 
@@ -233,20 +228,20 @@
 
   {{-- ****************************************************************************************** --}}
   {{-- ************************** Export To Excel Forms ***************************************** --}}
-  <form id="formExportArticles" method="POST" action="{{ route('exportArticles') }}" target="_blank">
+  <form id="formExportInventaires" method="POST" action="{{ route('exportInventaires') }}" target="_blank">
     @csrf
-    <input type="hidden" name="id_famille" id="export_id_famille">
-    <input type="hidden" name="id_site" id="export_id_site">
+    <input type="hidden" name="id_article_site" id="export_id_article">
+    <input type="hidden" name="id_zone" id="export_id_zone">
   </form>
 
   <script>
-  function exportArticlesFunction(){
-    let id_site = document.getElementById("filter_id_site").value;
-    let id_famille = document.getElementById("filter_id_famille").value;
+  function exportInventairesFunction(){
+    let id_article_site = document.getElementById("filter_id_article_site").value;
+    let id_zone = document.getElementById("filter_id_zone").value;
 
-    document.getElementById("export_id_site").value = id_site;
-    document.getElementById("export_id_famille").value = id_famille;
-    document.getElementById("formExportArticles").submit();
+    document.getElementById("export_id_article").value = id_article_site;
+    document.getElementById("export_id_zone").value = id_zone;
+    document.getElementById("formExportInventaires").submit();
   }
 </script>
 {{-- ************************** Export To Excel Forms ***************************************** --}}
@@ -262,7 +257,7 @@
     <input type="hidden" id="delete_id_article_site" name="id_article_site" />
   </form>
   <script>
-  function deleteArticleFunction(id_article,id_article_site, designation){
+  function deleteInventaireFunction(id_article,id_article_site, designation){
     var go = confirm('Vos êtes sur le point d\'effacer l\'article: "'+designation+'".\n voulez-vous continuer?');
     if(go){
       document.getElementById("delete_id_article").value = id_article;
@@ -270,17 +265,7 @@
       document.getElementById("formDeleteArticle").submit();
     }
   }
-  function updateArticleFunction(id_article,id_article_site,id_famille, id_site, id_unite, code, designation){
-    document.getElementById("formAddArticle").action = "{{ route('updateArticle') }}";
-    document.getElementById("submitButton").value = "Modifier";
-    document.getElementById("id_article").value = id_article;
-    document.getElementById("id_site").value = id_site;
-    document.getElementById("id_article_site").value = id_article_site;
-    document.getElementById("id_famille").value = id_famille;
-    document.getElementById("id_unite").value = id_unite;
-    document.getElementById("code").value = code;
-    document.getElementById("designation").value = designation;
-  }
+
   </script>
 </div>
 
@@ -344,6 +329,21 @@
     populateZone();
   });
 
+  function updateInventaireFuntion(id_inventaire,id_article_site,id_zone, date, nombre_palettes, nombre_pieces){
+    document.getElementById("formAddInventaire").action = "{{ route('updateInventaire') }}";
+    document.getElementById("submitButton").value = "Modifier";
+    document.getElementById("id_inventaire").value = id_inventaire;
+    document.getElementById("id_article_site").value = id_article_site;
+
+    document.getElementById("date").value = date;
+    document.getElementById("nombre_palettes").value = nombre_palettes;
+    document.getElementById("nombre_pieces").value = nombre_pieces;
+    $('.show-tick').selectpicker('refresh');
+    calculateTotal();
+    populateZone();
+    document.getElementById("id_zone").value = id_zone;
+  }
+
   function populateZone(){
     //liste des zones
     var zones = [];
@@ -356,7 +356,6 @@
     };
     zones.push(zone);
     @endforeach
-    console.log("Zones ===> "+zones);
     //--------------------
 
     //liste des articles
@@ -369,7 +368,6 @@
     };
     article_sites.push(item);
     @endforeach
-    console.log("article_sites ===> "+article_sites);
     //--------------------
 
     var s1 = document.getElementById("id_article_site");
@@ -386,7 +384,6 @@
         break;
       }
     }
-    console.log("selected_id_site ===> "+selected_id_site);
 
     //get zones from the selected id_site
     var myZones = [];
@@ -401,7 +398,6 @@
       var newOption = document.createElement("option");
       newOption.value = myZones[i].id_zone;
       newOption.innerHTML = myZones[i].libelle_zone;
-      //console.log(newOption);
       s2.options.add(newOption);
     }
   }
@@ -462,7 +458,6 @@
       }
     });
 
-
     var table = $('#inventairesTable').DataTable({
       dom: '<lf<Bt>ip>',
       lengthMenu: [
@@ -475,12 +470,12 @@
       info: false,
       stateSave: false,
       columnDefs: [
-        { targets: 0, width: "10%", type: "string", visible: true, searchable: true, orderable: true},
-        { targets: 1, width: "10%", type: "string", visible: true, searchable: true, orderable: true},
-        { targets: 2, width: "10%", type: "string", visible: true, searchable: true, orderable: true},
-        { targets: 3,  type: "string", visible: true, searchable: true, orderable: true},
-        { targets: 4, width: "10%", type: "string", visible: true, searchable: true, orderable: true},
-        { targets: 5, width: "05%", type: "string", visible: true, searchable: false, orderable: false},
+        { targets: 0, width: "", type: "string", visible: true, searchable: true, orderable: true},       //article
+        { targets: 1, width: "10%", type: "string", visible: true, searchable: true, orderable: true},    //zone
+        { targets: 2, width: "10%", type: "string", visible: true, searchable: true, orderable: true},    //date
+        { targets: 3, width: "", type: "string", visible: true, searchable: true, orderable: true},    //quantite
+        { targets: 4, width: "10%", type: "string", visible: true, searchable: true, orderable: true},    //cree par
+        { targets: 5, width: "05%", type: "string", visible: true, searchable: false, orderable: false},  //outils
       ],
     });
 
@@ -501,48 +496,12 @@
 
   });
   /*
-  $('#articlesTablea').DataTable({
-  dom: '<lf<Bt>ip>',
-  buttons: [
-  'copy', 'csv', 'excel', 'pdf', 'print',
-],
-lengthMenu: [
-[ 5, 10, 25, 50, -1 ],
-[ '5', '10', '25', '50', 'Tout' ]
-],
-columnDefs: [
-//{ targets:-1, width: "04%", visible: true, orderable: true, searchable: false},
-{ targets: 0, width: "10%", type: "string", visible: true, searchable: false, orderable: true},
-{ targets: 1, width: "10%", type: "string", visible: true, searchable: false, orderable: true},
-{ targets: 2, width: "10%", type: "string", visible: true, searchable: false, orderable: true},
-{ targets: 3,  type: "string", visible: true, searchable: false, orderable: true},
-{ targets: 4, width: "10%", type: "string", visible: true, searchable: false, orderable: true},
-{ targets: 5, width: "05%", type: "string", visible: true, searchable: false, orderable: true},
-],
-//order: [[ 0, "asc" ]],
-});
-
-$('a.toggle-vis').on('click', function (e) {
-e.preventDefault();
-var column = table.column($(this).attr('data-column'));
-column.visible(!column.visible());
-});
-
-table.columns().every(function () {
-var that = this;
-$('input', this.footer()).on('keyup change', function () {
-if (that.search() !== this.value) {
-that.search(this.value).draw();
-}
-});
-});*/
-/*
-function loadMore(){
-var page = $('.endless-pagination').data('next-page');
-if(page !== null) {
-$.get(page, function(data){
-$('.articlesHere').append(data.articles);
-$('.endless-pagination').data('next-page', data.next_page);
+  function loadMore(){
+  var page = $('.endless-pagination').data('next-page');
+  if(page !== null) {
+  $.get(page, function(data){
+  $('.articlesHere').append(data.articles);
+  $('.endless-pagination').data('next-page', data.next_page);
 });
 }
 else{
