bfccc56b8f72f942b161d9dce3eb477ddd03e61b
aminelagh <amine.laghlabi@gmail.com>
2018-04-29 23:57:22 +0100

    ouvrier

--- resources/views/ouvrier/layouts/top_left_menu.blade.php
+++ resources/views/ouvrier/layouts/top_left_menu.blade.php
@@ -1,12 +1,5 @@
 <div class="collapse navbar-collapse navbar-ex1-collapse">
   <ul class="nav navbar-nav">
     <li><a href="{{ route('ouvrier') }}">Accueil</a></li>
-    <li class='dropdown'>
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown"> Menu <b class="caret"></b></a>
-      <ul class="dropdown-menu">
-        <li><a href="#">Option 1</a></li>
-        <li><a href="#">Option 2</a></li>
-      </ul>
-    </li>
   </ul>
 </div>
